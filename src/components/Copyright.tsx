401fd84f3410874b4aa7080145fd01f7309ca0b8
IsmoilAbdujalilov <abdujalilovismoilgithub@gmail.com>
2024-05-21 11:08:32 +0500

    feat: Added

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -5,18 +5,8 @@ function Copyright() {
         <div className="row">
           <div className="col-lg-12">
             <p className="p-small">
-              Авторские права © 2020 <a href="https://inovatik.com">Иноватик</a>{" "}
-              - Все права защищены
-            </p>
-            <p className="p-small">
-              Распространяется:{" "}
-              <a
-                href="https://themewagon.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                ThemeWagon
-              </a>
+              Авторские права © {new Date().getFullYear()}{" "}
+              <a href="https://inovatik.com">Иноватик</a> - Все права защищены
             </p>
           </div>
         </div>
