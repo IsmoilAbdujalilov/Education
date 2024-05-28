6dc7e6ac5afc38ca81e563a18dc9c64d26fbfbea
IsmoilAbdujalilov <abdujalilovismoilgithub@gmail.com>
2024-05-28 12:57:51 +0500

    feat: Added codes

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,45 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const cardData = [
-  {
-    imgSrc: "https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp",
-    imgAlt: "Hollywood Sign on The Hill",
-    title: "Card title",
-    text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  },
-  {
-    imgSrc: "https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp",
-    imgAlt: "Palm Springs Road",
-    title: "Card title",
-    text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  },
-  {
-    imgSrc: "https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp",
-    imgAlt: "Los Angeles Skyscrapers",
-    title: "Card title",
-    text: "This is a longer card with supporting text below as a natural lead-in to additional content.",
-  },
-  {
-    imgSrc: "https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp",
-    imgAlt: "Skyscrapers",
-    title: "Card title",
-    text: "This is a longer card with supporting text below as a natural lead-in to additional content.",
-  },
-  {
-    imgSrc: "https://mdbcdn.b-cdn.net/img/new/standard/city/046.webp",
-    imgAlt: "Skyscrapers",
-    title: "Card title",
-    text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  },
-  {
-    imgSrc: "https://mdbcdn.b-cdn.net/img/new/standard/city/050.webp",
-    imgAlt: "Skyscrapers",
-    title: "Card title",
-    text: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
-  },
-];
-
 const CardList = () => {
   const [courses, setCourses] = useState([]);
 
@@ -48,8 +9,6 @@ const CardList = () => {
       .then((res) => res.json())
       .then((data) => setCourses(data.data));
   }, []);
-
-  console.log(courses);
 
   return (
     <div className="container">
